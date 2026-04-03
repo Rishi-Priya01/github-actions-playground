d1a2f1f9bc544217c8fdb922ac632bc34ee35c5c
Rishi-Priya01 <157196052+Rishi-Priya01@users.noreply.github.com>
2026-04-03 21:09:27 +0530

    Update mobile_steps.py

--- features/features/steps/mobile_steps.py
+++ features/features/steps/mobile_steps.py
@@ -2,11 +2,11 @@ from behave import given, when, then
 
 @given("I launch the mobile app")
 def step_launch_app(context):
-    print("Launching the mobile app...")
+    print("Launching mobile app...")
 
 @when("I enter valid credentials")
 def step_enter_credentials(context):
-    print("Entering username and password...")
+    print("Enter username and password...")
 
 @then("I should see the home screen")
 def step_verify_home(context):
